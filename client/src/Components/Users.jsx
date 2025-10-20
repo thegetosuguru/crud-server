ad6ad6017c0a82902c286dc242f9a3e947337f1d
Refat al hasan <refatalhasan911@gmail.com>
2025-10-20 22:22:43 +0600

    delete features added successfully

--- client/src/Components/Users.jsx
+++ client/src/Components/Users.jsx
@@ -4,7 +4,23 @@ import NavBar from './NavBar';
 
 const Users = () => {
 
-    const users = useLoaderData()
+    const users = useLoaderData();
+
+    const handleDelete = _id => {
+        console.log('delete', _id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deleteCount > 0){
+                alert('deleted successfully')
+            }
+            
+        })
+
+    }
 
     return (
         <div>
@@ -12,7 +28,10 @@ const Users = () => {
             <div className='flex flex-col h-fit w-full items-center justify-center'>
                 <h2>{users.length}</h2>
                 {
-                    users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                    users.map(user => <p key={user._id}
+                    >{user.name} : {user.email} : {user._id}
+                    <button onClick={() => handleDelete(user._id)} className='btn m-2 text-red-500'>X</button>
+                    </p>)
                 }
             </div>
         </div>
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,14 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log('please this id', id);
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
